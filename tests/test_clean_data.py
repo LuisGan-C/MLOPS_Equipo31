b8c6da8d425b6800a0e70cd2497f3a250bae08d2
Alejandro Ahedo <alejandro.ahedo.glez@gmail.com>
2025-11-13 20:34:23 -0600

    Se crea repositorio tests y el archivo test_clean_data.py

---
+++ tests/test_clean_data.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from train import clean_data
+
+def test_clean_data_removes_nulls_and_invalids():
+    df = pd.DataFrame({
+        "col1": [1, 2, None],
+        "col2": ["ok", " ?", "null"]
+    })
+    cleaned = clean_data(df)
+    assert cleaned.isnull().sum().sum() == 0
+    assert not cleaned.isin(["?", "error", "NAN", "invalid", "null"]).any().any()
